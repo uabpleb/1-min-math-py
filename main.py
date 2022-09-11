85046a8a7ac57da1d8891fcd319256f6d4100cef
uabpleb <uabpleb.goldfish699@slmails.com>
2022-09-11 16:41:54 +0200

    async attempt failure

--- main.py
+++ main.py
@@ -1,8 +1,53 @@
-timer = 60
+import asyncio
+import time
+import random
+import operation as op
 
-while timer:
-    #show math
-    #await input
-    #check result
-    timer -= 1 #somewhere... async
+MATH_OPERATORS = ['+', '-', '*', '/']
+
+def main():
+    done = False
+    #done = await clock()
+
+    """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(clock())
+    done = loop.run_until_complete(clock())
+    """
     
+
+    correct = 0
+    while not done:
+        done = asyncio.create_task(clock())
+        """
+        operation = next_op()
+        operation.show()
+        response = input("\n")
+
+        if float(response) == float(operation.perform()):
+            correct += 1
+        """
+        input("testeando: ")
+        correct += 1
+    print("Correct answers: " + str(correct))
+        
+
+async def clock()->bool:
+    print("starting clock...")
+    await asyncio.sleep(5)
+    print("finished clock.")
+
+    return True
+
+def next_op() -> op.Operation:
+    op1 = random.randint(0, 10)
+    op2 = random.randint(1, 10)
+    operator = random.choice(MATH_OPERATORS)
+    
+    return op.Operation(op1, op2, operator)
+
+
+if __name__ == "__main__":
+    #asyncio.run(main())
+    main()
---
+++ operation.py
@@ -0,0 +1,21 @@
+
+class Operation:
+    def __init__(self, op1, op2, operator):
+        self.op1 = op1
+        self.op2 = op2
+        self.operator = operator
+
+    def perform(self):
+        if self.operator == '+':
+            return self.op1 + self.op2
+        elif self.operator == '-':
+            return self.op1 - self.op2
+        elif self.operator == '*':
+            return self.op1 * self.op2
+        elif self.operator == '/':
+            return self.op1 / self.op2
+        else:
+            raise Exception("bad operator")
+    
+    def show(self):
+        print(str(self.op1) + self.operator + str(self.op2))
